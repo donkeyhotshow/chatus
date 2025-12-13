753115133fad3f32a257a911fe6cf5ccc23b4d10
RYBA <150953231+donkeyhotshow@users.noreply.github.com>
2025-12-13 23:07:20 +0200

    Update next.config.js for TypeScript and ESLint settings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
